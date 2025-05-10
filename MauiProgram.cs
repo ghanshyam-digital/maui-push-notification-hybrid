cce05cdbf9c6335e73665c806a7f5ad4fc61f4a5
Pratik-Palanpure <pratik@ghanshyamdigital.com>
2025-05-10 12:37:45 +0530

    - Add require packages and handle notification in home.razor.

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,16 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using MauiNotificationApp.Interfaces;
+using MauiNotificationApp.Services;
+using Microsoft.Extensions.Logging;
+using Microsoft.Maui.LifecycleEvents;
+using Plugin.Firebase.Auth;
+using Plugin.Firebase.Bundled.Shared;
+using Plugin.Firebase.CloudMessaging;
+
+#if IOS
+using Plugin.Firebase.Bundled.Platforms.iOS;
+#elif ANDROID
+using Plugin.Firebase.Bundled.Platforms.Android;
+#endif
 
 namespace MauiPushNotificationHybrid
 {
@@ -9,6 +21,7 @@ namespace MauiPushNotificationHybrid
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .RegisterFirebaseServices()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
@@ -16,12 +29,49 @@ namespace MauiPushNotificationHybrid
 
             builder.Services.AddMauiBlazorWebView();
 
+            builder.Services.AddHttpClient();
+
+            builder.Services.AddSingleton<INotificationService, NotificationService>();
+
 #if DEBUG
-    		builder.Services.AddBlazorWebViewDeveloperTools();
-    		builder.Logging.AddDebug();
+            builder.Services.AddBlazorWebViewDeveloperTools();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
         }
+
+        private static MauiAppBuilder RegisterFirebaseServices(this MauiAppBuilder builder)
+        {
+            builder.ConfigureLifecycleEvents(events => {
+#if IOS
+            events.AddiOS(iOS => iOS.WillFinishLaunching((app, launchOptions) => {
+                CrossFirebase.Initialize(CreateCrossFirebaseSettings());
+                return false;
+            }));
+#elif ANDROID
+                events.AddAndroid(android => android.OnCreate((activity, _) => {
+                    var settings = CreateCrossFirebaseSettings();
+                    CrossFirebase.Initialize(activity, settings);
+                }));
+#endif
+            });
+
+            builder.Services.AddSingleton(_ => CrossFirebaseAuth.Current);
+            builder.Services.AddSingleton(_ => CrossFirebaseCloudMessaging.Current);
+
+
+            return builder;
+        }
+
+        private static CrossFirebaseSettings CreateCrossFirebaseSettings()
+        {
+            return new CrossFirebaseSettings(
+                isAuthEnabled: true,
+                isCloudMessagingEnabled: true,
+                isCrashlyticsEnabled: true
+            );
+        }
     }
+
 }
